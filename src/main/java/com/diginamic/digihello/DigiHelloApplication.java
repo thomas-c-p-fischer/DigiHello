cb9dc85245fa6f6acd33fb7af8b57d89028b609d
thomas.fischer <thomas.fischer@sully-group.com>
2024-04-08 12:43:45 +0200

    Commit initial

---
+++ src/main/java/com/diginamic/digihello/DigiHelloApplication.java
@@ -0,0 +1,13 @@
+package com.diginamic.digihello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigiHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diginamic/digihello/DigiHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diginamic.digihello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
